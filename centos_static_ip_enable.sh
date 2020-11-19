f440d79b702eac29c391da3f8160da9b9ae2bc74
SergZavh <ssss@ddd.ddd>
2020-11-19 13:10:20 +0200

    Edit scripts

--- centos_static_ip_enable.sh
+++ centos_static_ip_enable.sh
@@ -3,22 +3,25 @@
 if [ -f "/etc/sysconfig/network-scripts/ifcfg-enp0s8" ]; then
    echo "File exist"
 else
-   cat > /etc/netplan/my_settings.yaml <<EOF
+   cat > /etc/sysconfig/network-scripts/ifcfg-enp0s8 <<EOF
 DEVICE="enp0s8"
 BOOTPROTO="static"
 NM_CONTROLLED="yes"
 ONBOOT="yes"
 TYPE="Ethernet"
-IPADDR="192.168.56.103"
+IPADDR="192.168.56.108"
 NETMASK="255.255.255.0"
 GATEWAY="192.168.56.1"
-DNS1="8.8.8.8"
+DNS1="192.168.56.108"
 
 EOF
-
+   ifdown enp0s8
+   sleep 3
+   ifup enp0s8
    systemctl restart NetworkManager
-   nmcli networking off
-   nmcli networking on
+#   nmcli networking off
+#   nmcli networking on
+
 fi
 
-apt install -y openssh-server
+#dnf install -y openssh-server
